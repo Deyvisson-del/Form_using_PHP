218f0593beafa6659afc49321c4a9ab09429287b
Deyvisson-del <deyvissonrafael018@gmail.com>
2025-04-11 14:26:22 -0300

    second commit

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log("Servidor node funcionando")
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Olá, mundo do Node com Express!');
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
